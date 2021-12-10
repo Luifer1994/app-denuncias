e81af010d512ce34dc5c63c55ab0d42ffd921a12
manuel <maangulo@indracompany.com>
2021-12-09 21:48:07 -0500

    firebaase config

---
+++ src/utils/firebase.js
@@ -0,0 +1,44 @@
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCT0XidrRC25UAUmlUXZnbY8P2GMmW8jVw",
+  authDomain: "denuncias-ronald.firebaseapp.com",
+  projectId: "denuncias-ronald",
+  storageBucket: "denuncias-ronald.appspot.com",
+  messagingSenderId: "908342821134",
+  appId: "1:908342821134:web:231378adf6221a690a175f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const  defaultStorage = getStorage(app);
+
+export const  UploadFile = (file)=>{
+
+
+    return Promise( resolve=>{
+        let ref =   defaultStorage.ref("responses-files");
+       let task =  ref.child(file.name).put(file);
+       task.on('state_changed', function(snapshot){
+        console.log("snap",snapshot);
+      }, function(error) {
+        console.log("upload error",error.message);
+      }, function() {
+        // Handle successful uploads on complete
+        // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+        uploadTask.snapshot.ref.getDownloadURL().then(function(downloadURL) {
+            resolve(downloadURL);
+          console.log('File available at', downloadURL);
+        });
+      });
+
+        
+    } );
+
+
+
+}
