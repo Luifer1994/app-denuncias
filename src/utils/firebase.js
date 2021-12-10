ca52e342558841e9688c6b9180424bb28350602d
manuel <maangulo@indracompany.com>
2021-12-09 22:52:09 -0500

    storage config

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,7 +1,6 @@
-import firebase from 'firebase/compat/app';
-import { initializeApp } from "firebase/app"
-import 'firebase/compat/auth';
-import 'firebase/compat/firestore';
+
+import firebase  from "firebase/compat/app"
+import 'firebase/compat/storage';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -15,16 +14,17 @@ const firebaseConfig = {
   appId: "1:908342821134:web:231378adf6221a690a175f"
 };
 
+
 // Initialize Firebase
-const firebaseApp  = initializeApp(firebaseConfig);
-const  defaultStorage = firebaseApp.storage();
+ firebase.initializeApp(firebaseConfig);
+const  defaultStorage = firebase.storage();
 
 export const  UploadFile = (file)=>{
 
 
     return new Promise( resolve=>{
-        let ref =   defaultStorage.ref("responses-files");
-       let task =  ref.child(file.name).put(file);
+        let customRef =   defaultStorage.ref("responses-files");
+       let task =  customRef.child(file.name).put(file);
        task.on('state_changed', function(snapshot){
         console.log("snap",snapshot);
       }, function(error) {
